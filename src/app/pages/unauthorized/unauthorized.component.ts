55f5b0b7e3ec9411612038b776d020dd23b7f9e3
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-06-08 07:07:42 +0300

    feat: update UnauthorizedComponent to use environment variable for support email link

--- src/app/pages/unauthorized/unauthorized.component.ts
+++ src/app/pages/unauthorized/unauthorized.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { MessageModule } from 'primeng/message';
 import { Location } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-unauthorized',
@@ -13,6 +14,8 @@ import { Location } from '@angular/common';
 })
 export class UnauthorizedComponent {
   private location = inject(Location);
+
+  readonly environment = environment;
 
   goBack(): void {
     this.location.back();
