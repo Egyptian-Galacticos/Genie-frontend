12371c9bd1f9e764a11a9221526fcce51d0d8bcb
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-06-25 10:52:23 +0300

    feat: implement product service with CRUD operations and image handling

---
+++ src/app/features/shared/services/product.service.ts
@@ -0,0 +1,43 @@
+import { ApiResponse, PaginatedResponse } from './../../../core/interfaces/api.interface';
+import { AuthService } from './../../../core/auth/services/auth.service';
+import { IProduct } from '../utils/interfaces';
+import { ApiService } from './../../../core/services/api.service';
+import { inject, Injectable } from '@angular/core';
+import { RequestOptions } from '../../../core/interfaces/api.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private readonly apiService = inject(ApiService);
+  private readonly authService = inject(AuthService);
+
+  createProduct(dto: FormData) {
+    return this.apiService.post<ApiResponse<IProduct>>('products', dto);
+  }
+  getMyProducts(RequestOptions: RequestOptions) {
+    return this.apiService.get<PaginatedResponse<IProduct>>('seller/products', RequestOptions);
+  }
+  updateProductActiveStatus(id: number, isActive: boolean) {
+    return this.apiService.patch<ApiResponse>(`products/${id}`, { is_active: isActive });
+  }
+  updateProductFeaturedStatus(id: number, isFeatured: boolean) {
+    return this.apiService.patch<ApiResponse>(`products/${id}`, { is_featured: isFeatured });
+  }
+  DeleteProduct(id: number) {
+    return this.apiService.delete<ApiResponse>(`products/${id}`);
+  }
+  getProduct(slug: string) {
+    return this.apiService.get<ApiResponse<IProduct>>(`products/${slug}`);
+  }
+  updateProduct(id: number, dto: FormData) {
+    dto.append('_method', 'PUT');
+    return this.apiService.post<ApiResponse>(`products/${id}`, dto);
+  }
+  getAllTags() {
+    return this.apiService.get<ApiResponse<string[]>>('products/tags/all');
+  }
+  deleteImage(slug: string, id: number, collection: string, mediaId: number) {
+    return this.apiService.delete<ApiResponse>(`products/${slug}/media/${collection}/${mediaId}`);
+  }
+}
