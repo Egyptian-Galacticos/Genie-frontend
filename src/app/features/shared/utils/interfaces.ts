c42ab03188329326ecf3efb96c0da9788114f1ae
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-06-25 12:25:58 +0300

    feat: update product and category interfaces with new properties and structure

--- src/app/features/shared/utils/interfaces.ts
+++ src/app/features/shared/utils/interfaces.ts
@@ -29,16 +29,6 @@ export interface Address {
   zip_code: string;
 }
 
-export interface IProduct {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  quantity: number;
-  image: string;
-  category: string;
-}
-
 export interface CustomTableLazyLoadEvent extends TableLazyLoadEvent {
   customProperty?: string;
 }
@@ -57,26 +47,143 @@ export interface dataTableColumn {
 /**quotation interfaces */
 
 export interface CreateQuoteDto {
-  quote_request_id: string;
+  quote_request_id: number;
   buyer_id: string;
   seller_message: string;
-  quote_items?: CreateQuoteItemDto[];
+  items?: CreateQuoteItemDto[];
+  rfq_id?: number;
+  conversation_id?: number;
 }
 
 export interface CreateQuoteItemDto {
-  product_id: string;
+  product_id: number;
   quantity: number;
   unit_price: number;
   notes: string;
 }
 
 export interface IRequestForQuote {
-  id: string;
+  id: number;
   quote: string;
   status: string;
   date: string;
   buyer: ICompany;
-  product: Partial<IProduct>;
+  initial_product: Partial<IProduct>;
   quantity?: number;
   unit_price?: number;
+}
+
+//products
+export interface createProductDto {
+  brand?: string;
+  currency: string;
+  description: string;
+  dimensions: {
+    length: number;
+    width: number;
+    height: number;
+  };
+  hs_code?: string;
+  model_number?: string;
+  name: string;
+  origin: string;
+  price: number;
+  price_tiers:
+    | {
+        from_quantity: number | null;
+        to_quantity: number | null;
+        price: number | null;
+      }[]
+    | null;
+  sample_available: boolean;
+  sample_price?: number;
+  sku: string;
+  tags?: string[] | null;
+  weight: number;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  description: string;
+  slug: string;
+  status: 'active' | 'inactive' | string;
+  icon: string;
+  icon_url: string;
+  image_url: string;
+  thumbnail_url: string;
+  level: number;
+  parent_id: number | null;
+  parent: Category | null;
+  path: string | null;
+  children: Category[];
+  creator: {
+    id: number;
+    full_name: string;
+  };
+  updater: {
+    id: number;
+    full_name: string;
+  } | null;
+  created_at: string;
+  updated_at: string;
+  seo_metadata: {
+    title: string;
+    description: string;
+    keywords: string;
+    og_title: string;
+    og_description: string;
+  };
+}
+
+export interface ImageFile {
+  id: number;
+  name: string;
+  file_name: string;
+  url: string;
+  thumbnail_url: string;
+  size: string;
+  mime_type: string;
+}
+
+export interface IProduct {
+  id: number;
+  brand: string;
+  model_number: string;
+  name: string;
+  slug: string;
+  description: string;
+  price: string;
+  currency: string;
+  is_featured: boolean;
+  is_active: boolean;
+  is_approved: boolean;
+  sample_available: boolean;
+  sample_price: number;
+  seller: string;
+  main_image: ImageFile;
+  images: ImageFile[];
+  hs_code: string;
+  sku: string;
+  origin: string;
+  weight: string;
+  dimensions: {
+    length: number;
+    width: number;
+    height: number;
+  };
+  tiers: {
+    from_quantity: number;
+    to_quantity: number;
+    price: number;
+  }[];
+  tags: string[];
+  category: Category;
+  category_id: number;
+}
+export interface CreateCategoryInProduct {
+  id: number;
+  parent_id: number;
+  parent: CreateCategoryInProduct;
+  name: string;
 }
