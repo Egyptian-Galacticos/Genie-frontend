ce2d188776a5e17be2c717dff5833643df5674e9
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-06-25 10:54:41 +0300

    fix: update quotes table to use correct metadata and improve error handling

--- src/app/features/seller-dashboard/quotes-requests/quotes-requests.component.ts
+++ src/app/features/seller-dashboard/quotes-requests/quotes-requests.component.ts
@@ -13,6 +13,7 @@ import { DataTableComponent } from '../../shared/data-table/data-table.component
 import { CreateQuoteModalComponent } from '../../shared/create-quote-modal/create-quote-modal.component';
 import { QuotesService } from '../../shared/services/quotes.service';
 import { ToastModule } from 'primeng/toast';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-quotes',
@@ -28,6 +29,7 @@ import { ToastModule } from 'primeng/toast';
     CreateQuoteModalComponent,
     CreateQuoteModalComponent,
     ToastModule,
+    DatePipe,
   ],
   templateUrl: './quotes-requests.component.html',
   styleUrl: './quotes-requests.component.css',
@@ -54,25 +56,12 @@ export class QuotesRequestsComponent {
     this.quoteService.getCurrentSellerQuoteRequest(requestOptions).subscribe({
       next: (response: PaginatedResponse<IRequestForQuote>) => {
         this.quotesResponse = response;
+        this.quotesLoading = false;
       },
       error: error => {
-        console.log(error);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: "Couldn't load quotes",
-          life: 5000,
-        });
-        this.quotesResponse = {
-          success: false,
-          data: this.quotesPlaceholder,
-          pagination: {
-            page: 1,
-            limit: 10,
-            total: 6,
-            totalPages: 1,
-          },
-        };
+        console.error(error);
+        this.showError("Couldn't load quotes");
+
         this.quotesLoading = false;
       },
     });
@@ -117,25 +106,22 @@ export class QuotesRequestsComponent {
     { field: 'actions', header: 'Actions', filterableColumn: false, filterType: 'clear' },
   ];
 
-  getStatusSeverity(status: string): string {
+  getStatusSeverity(status: string): 'warn' | 'success' | 'danger' | 'secondary' | 'info' {
     switch (status.toLowerCase()) {
-      case 'new':
-        return 'info';
       case 'pending':
-        return 'warning';
-      case 'approved':
+        return 'warn';
+      case 'quoted':
+      case 'seen':
         return 'success';
-      case 'canceled':
       case 'rejected':
         return 'danger';
       default:
-        return 'info';
+        return 'secondary';
     }
   }
 
   // open create quote modal
   openCreateQuoteModal(quoteRequest: IRequestForQuote) {
-    console.log(quoteRequest, this.createQuoteModalVisible());
     this.beingQuotedQuoteRequest = quoteRequest;
     this.createQuoteModalVisible.set(true);
   }
@@ -150,17 +136,24 @@ export class QuotesRequestsComponent {
           detail: 'Quote sent successfully',
           life: 5000,
         });
-        this.loadQuoteRequests(this.currentRequestOptions);
+        this.quoteService
+          .updateQuoteRequestStatus({
+            id: this.beingQuotedQuoteRequest.id,
+            status: 'Quoted',
+          })
+          .subscribe({
+            next: () => {
+              this.beingQuotedQuoteRequest.status = 'Quoted';
+            },
+            error: () => {
+              this.showError("Couldn't mark quote request as quoted");
+            },
+          });
         this.creatingQuote = false;
         this.createQuoteModalVisible.set(false);
       },
       error: () => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: "Couldn't send quote",
-          life: 5000,
-        });
+        this.showError("Couldn't send quote");
         this.creatingQuote = false;
       },
     });
@@ -172,12 +165,7 @@ export class QuotesRequestsComponent {
         quoteRequest.status = 'seen';
       },
       error: () => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: "Couldn't mark quote request as seen",
-          life: 5000,
-        });
+        this.showError("Couldn't mark quote request as seen");
       },
     });
   }
@@ -198,180 +186,11 @@ export class QuotesRequestsComponent {
         },
       });
   }
-  quotesPlaceholder = [
-    {
-      id: '1',
-      quote: 'Quote 1',
-      status: 'canceled',
-      date: '2023-01-02',
-      buyer: {
-        id: '1',
-        user_id: '1',
-        name: 'Buyer 1',
-        logo: 'https://via.placeholder.com/150',
-        description: 'Description 1',
-        website: 'https://example.com',
-        email: 'q9l5M@example.com',
-        tax_id: '123456789',
-        company_phone: '123456789',
-        commercial_registration: '123456789',
-        address: {
-          street: '123 Main St',
-          city: 'Anytown',
-          state: 'CA',
-          country: 'USA',
-          zip_code: '12345',
-        },
-      },
-      product: {
-        id: '1',
-        name: 'Product 1',
-      },
-    },
-    {
-      id: '2',
-      quote: 'Quote 2',
-      status: 'New',
-      date: '2023-01-05',
-      buyer: {
-        id: '1',
-        user_id: '1',
-        name: 'Buyer 1',
-        logo: 'https://via.placeholder.com/150',
-        description: 'Description 1',
-        website: 'https://example.com',
-        email: 'q9l5M@example.com',
-        tax_id: '123456789',
-        company_phone: '123456789',
-        commercial_registration: '123456789',
-        address: {
-          street: '123 Main St',
-          city: 'Anytown',
-          state: 'CA',
-          country: 'USA',
-          zip_code: '12345',
-        },
-      },
-      product: {
-        id: '2',
-        name: 'Product 2',
-      },
-    },
-    {
-      id: '3',
-      quote: 'Quote 3',
-      status: 'New',
-      date: '2023-01-04',
-      buyer: {
-        id: '1',
-        user_id: '1',
-        name: 'Buyer 1',
-        logo: 'https://via.placeholder.com/150',
-        description: 'Description 1',
-        website: 'https://example.com',
-        email: 'q9l5M@example.com',
-        tax_id: '123456789',
-        company_phone: '123456789',
-        commercial_registration: '123456789',
-        address: {
-          street: '123 Main St',
-          city: 'Anytown',
-          state: 'CA',
-          country: 'USA',
-          zip_code: '12345',
-        },
-      },
-      product: {
-        id: '3',
-        name: 'Product 3',
-      },
-    },
-    {
-      id: '4',
-      quote: 'Quote 4',
-      status: 'New',
-      date: '2023-01-10',
-      buyer: {
-        id: '1',
-        user_id: '1',
-        name: 'Buyer 1',
-        logo: 'https://via.placeholder.com/150',
-        description: 'Description 1',
-        website: 'https://example.com',
-        email: 'q9l5M@example.com',
-        tax_id: '123456789',
-        company_phone: '123456789',
-        commercial_registration: '123456789',
-        address: {
-          street: '123 Main St',
-          city: 'Anytown',
-          state: 'CA',
-          country: 'USA',
-          zip_code: '12345',
-        },
-      },
-      product: {
-        id: '4',
-        name: 'Product 4',
-      },
-    },
-    {
-      id: '5',
-      quote: 'Quote 5',
-      status: 'New',
-      date: '2023-01-09',
-      buyer: {
-        id: '1',
-        user_id: '1',
-        name: 'Buyer 1',
-        logo: 'https://via.placeholder.com/150',
-        description: 'Description 1',
-        website: 'https://example.com',
-        email: 'q9l5M@example.com',
-        tax_id: '123456789',
-        company_phone: '123456789',
-        commercial_registration: '123456789',
-        address: {
-          street: '123 Main St',
-          city: 'Anytown',
-          state: 'CA',
-          country: 'USA',
-          zip_code: '12345',
-        },
-      },
-      product: {
-        id: '5',
-        name: 'Product 5',
-      },
-    },
-    {
-      id: '6',
-      quote: 'Quote 6',
-      status: 'New',
-      date: '2023-01-08',
-      buyer: {
-        id: '1',
-        user_id: '1',
-        name: 'Buyer 1',
-        logo: 'https://via.placeholder.com/150',
-        description: 'Description 1',
-        website: 'https://example.com',
-        email: 'q9l5M@example.com',
-        tax_id: '123456789',
-        company_phone: '123456789',
-        commercial_registration: '123456789',
-        address: {
-          street: '123 Main St',
-          city: 'Anytown',
-          state: 'CA',
-          country: 'USA',
-          zip_code: '12345',
-        },
-      },
-      product: {
-        id: '6',
-        name: 'Product 6',
-      },
-    },
-  ];
+  private showError(message: string): void {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: message,
+    });
+  }
 }
